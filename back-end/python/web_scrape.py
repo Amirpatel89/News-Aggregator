86a6460a046cb547838591ca4f76783b819c5e97
Amir Patel <amirpatel89@gmail.com>
2018-01-09 09:09:57 -0500

    new work

--- back-end/python/web_scrape.py
+++ back-end/python/web_scrape.py
@@ -21,42 +21,74 @@ cursor = conn.cursor()
 # split_pic = (final_pic.split('CompressAll300/'))[1]
 # print(split_pic)
 item_container = soup.find_all('div', class_='item-container')
-# if item-container.find_all('a', class_='item-title').text
-	# item_picture = 'http:' + item_container.find('img', class_='lazy-img')['data-src']
-		# item_name = item_container.get('a', class_='item-title').text
-	# item_price = '$' + item_container.find('li', class_='price-current').find('strong').text + '.99'
-	# item_shipping = (item_container.find('li', class_='price-ship').text).strip()
-for item_name in item_container:
-	name = item_name.find_all('a', class_='item-title')
-	print (name[0].text)
-# except Exception as e:
-# 	item_container = None
-# 	item_title = None
-# 	item_picture = None
-# 	item_price = None
-# 	item_shipping = None
+
+
+def get_data():
+	lists = []
+	for index, item_name in enumerate(item_container):
+		name = item_name.find_all('a', class_='item-title')[0].text
+		lists.append({'name': name})
+		lists[index]['index'] = index
+		
+	# for listing in item_container:
+	# 	names['name: %s' % listing.find_all('a', class_='item-title')[0].text] = ['name']
+		# print(name)
+	for index, item_price in enumerate(item_container):
+		price = item_price.find('li', class_='price-current').find('strong')
+		if price == None:
+			# print('Not Available')
+			price == ('Not Available')
+			lists[index]['price'] = price
+
+
+		else:
+			# print('$' + price.text +'.99')
+			price = ('$' + price.text +'.99')
+			prices = []
+		lists[index]['price'] = price
+		# print(lists)
+	for index, item_picture in enumerate(item_container):
+			picture = 'http:' + item_picture.find('img', class_='lazy-img')['data-src']
+			# print (picture)
+			lists[index]['picture'] = picture
+			# print(lists)
+	for index, item_shipping in enumerate(item_container):
+			shipping = (item_shipping.find('li', class_='price-ship').text).strip()
+			lists[index]['shipping'] = shipping
+			# print(lists[0]['name'])
+
+
+
+
+
+
+
 	
-# print(item_container)
-# print(len(item_name))
-# print(item_picture)
-# print(item_name.text)
-
-# print(item_price)
-# print(item_shipping)
-
-# add_item = ("INSERT INTO newegg "
-# 			"(id, itemname, itempic, itemprice, itemshipping) "
-# 			"VALUES (%s, %s, %s, %s, %s)")
-
-# data_item = ('1', item_name, item_picture, item_price, item_shipping)
+	
+	def create_table():
+		# map(lists, range(0, len(lists[index])))
+		val_index = lists[index]['index']
+		val_name = lists[index]['name']
+		val_picture = lists[index]['picture']
+		val_price = lists[index]['price']
+		val_shipping = lists[index]['shipping']
 
 
-# cursor.execute("DELETE FROM newegg ")
-# conn.commit()
-# cursor.execute(add_item, data_item)  
-# conn.commit()
+		add_item = ("INSERT INTO newegg "
+					"(id, itemname, itempic, itemprice, itemshipping) "
+					"VALUES (%s, %s, %s, %s, %s)")
 
-cursor.close() 
-conn.close()                                                                  
+		data_item = (val_index, val_name, val_picture, val_price, val_shipping)
 
+
+		cursor.execute("DELETE FROM newegg ")
+		conn.commit()
+		cursor.execute(add_item, data_item)  
+		conn.commit()
+
+		cursor.close() 
+		conn.close()                                                                  
+
+	create_table();
+get_data()
 
